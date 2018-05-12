05a8392e46201807bd18bf9e7490689e2649317e
Tony Zhang <v_equals_ayia@hotmail.com>
2018-05-12 22:43:55 +1000

    12.5.18 - Games JS

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+// JavaScript for Tony Zhang's Website
+
+function showHide(n) {
+    // Toggles display between tile info when the tile is clicked
+    var infos = document.getElementsByClassName("information");
+
+    for(var i = 0; i < infos.length; i++){
+        infos[i].style.display = "none";
+    }
+
+    document.getElementById("t"+(n+1)).style.display = "flex";
+    document.getElementById("t0").style.display = "flex";
+}   
